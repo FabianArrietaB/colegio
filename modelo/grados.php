84afc58311ca0c8ded9284ce371358737c52ed7b
FabianArrietaB <fabianarrieta03@gmail.com>
2023-04-16 09:18:25 -0500

    1604

--- modelo/grados.php
+++ modelo/grados.php
@@ -18,5 +18,63 @@
             return $respuesta;
         }
 
+        public function agregargrado($datos){
+            $conexion = Conexion::conectar();
+            $sql = "INSERT INTO grados (id_categoria, gra_nombre, gra_precio ) VALUES(?, ?, ?)";
+            $query = $conexion->prepare($sql);
+            $query->bind_param("iss", $datos['idcat'], $datos['nombre'], $datos['precio']);
+            $respuesta = $query->execute();
+            return $respuesta;
+        }
+
+        public function detallegrado($idgrado){
+            $conexion = Conexion::conectar();
+            $sql ="SELECT
+                grados.id_grado         AS idgrado,
+                grados.gra_nombre       AS nombre,
+                grados.gra_matric       AS matric,
+                grados.gra_pensio       AS pensio,
+                grados.gra_canalu       AS canalu,
+                grados.gra_fecope       AS fecha,
+                grados.gra_estado       AS estado,
+                empleados.id_empleado   AS idempleado,
+                empleados.emp_nombre    AS nompro
+                FROM grados AS grados
+                INNER JOIN empleados AS empleados ON grados.id_empleado = empleados.id_empleado
+                WHERE grados.id_grado ='$idgrado'";
+            $respuesta = mysqli_query($conexion,$sql);
+            $grados = mysqli_fetch_array($respuesta);
+            $datos = array(
+            'idgrado' => $grados['idgrado'],
+            'nombre' => $grados['nombre'],
+            'matric' => $grados['matric'],
+            'pensio' => $grados['pensio'],
+            'idempleado' => $grados['idempleado'],
+            'nompro' => $grados['nompro'],
+            'fecha' => $grados['fecha']
+            );
+            return $datos;
+        }
+
+        public function editargrado($datos){
+            $conexion = Conexion::conectar();
+            $sql = "UPDATE grados SET id_categoria = ?, gra_nombre = ?, gra_precio = ?, gra_fecope = ? WHERE id_grado = ?";
+            $query = $conexion->prepare($sql);
+            $query->bind_param('isssi', $datos['idcat'], $datos['nombre'], $datos['precio'], $datos['fecha'], $datos['idgrado']);
+            $respuesta = $query->execute();
+            $query->close();
+            return $respuesta;
+        }
+
+        public function eliminargrado($idgrado){
+            $conexion = Conexion::conectar();
+            $sql = "DELETE FROM grados WHERE id_grado=?";
+            $query = $conexion->prepare($sql);
+            $query->bind_param('i', $idgrado);
+            $respuesta = $query->execute();
+            $query->close();
+            return $respuesta;
+        }
+
     }
 ?>
