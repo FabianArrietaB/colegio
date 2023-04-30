0e11554cef2fd6ac5ead661ea6f1598f63b35841
FabianArrietaB <fabianarrieta03@gmail.com>
2023-04-29 19:43:41 -0500

    new

--- modelo/alumnos.php
+++ modelo/alumnos.php
@@ -33,13 +33,22 @@
             $insertalumno = "INSERT INTO alumnos (id_grado, alu_nombre, alu_cladoc, alu_docume, alu_sexo, alu_gposan, alu_factrh, alu_ciudad, alu_direcc, alu_estrat, alu_telcel, alu_correo, alu_fecnac)
                                 VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             $query = $conexion->prepare($insertalumno);
-            $query->bind_param("isssssssssssssssssssss", $datos['idgrado'], $datos['nombre'], $datos['cladoc'], $datos['docume'], $datos['sexo'], $datos['gposan'], $datos['factrh'], $datos['ciudad'], $datos['direcc'], $datos['estrat'], $datos['telcel'], $datos['correo'], $datos['fecnac'],);
+            $query->bind_param("issssssssssss", $datos['idgrado'], $datos['nombre'], $datos['cladoc'], $datos['docume'], $datos['sexo'], $datos['gposan'], $datos['factrh'], $datos['ciudad'], $datos['direcc'], $datos['estrat'], $datos['telcel'], $datos['correo'], $datos['fecnac'],);
             $respuesta = $query->execute();
             $idalumno = mysqli_insert_id($conexion);
-            $sql = "INSERT INTO acudientes( id_alumno, acu_nombre, acu_cladoc, acu_docume, acu_ciudad, acu_direcc, acu_telcel, acu_correo, acu_parent) VALUES($idalumno, $datos['nommad]'' $datos['cldoma]'' $datos['docmad]'' $datos['ciumad]'' $datos['dirmad]'' $datos['telmad]'' $datos['cormad]'' $datos['parmad]''";
-            $sql = "INSERT INTO acudientes( id_alumno, acu_nombre, acu_cladoc, acu_docume, acu_ciudad, acu_direcc, acu_telcel, acu_correo, acu_parent) VALUES($idalumno, $datos['nompad]'' $datos['cldopa]'' $datos['docpad]'' $datos['ciupad]'' $datos['dirpad]'' $datos['telpad]'' $datos['corpad]'' $datos['parpad]''";
-            $sql = "INSERT INTO matriculas( id_alumno, id_grado, mat_valmat, mat_pensio, mat_saldo, mat_detalle) VALUES($idalumno, $datos['idgrado'], $datos['matric]'' $datos['pensio]'' $datos['abono]'',$datos['detall]''";
-            $query = $conexion->prepare($sql);
+            $insertmadre = "INSERT INTO acudientes( id_alumno, acu_nombre, acu_cladoc, acu_docume, acu_ciudad, acu_direcc, acu_telcel, acu_correo, acu_parent) 
+                            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $query = $conexion->prepare($insertmadre);
+            $query->bind_param("issssssss", $idalumno, $datos['nommad'], $datos['cldoma'], $datos['docmad'], $datos['ciumad'], $datos['dirmad'], $datos['telmad'], $datos['cormad'], $datos['parmad'],);
+            $respuesta = $query->execute();
+            $insertpadre = "INSERT INTO acudientes( id_alumno, acu_nombre, acu_cladoc, acu_docume, acu_ciudad, acu_direcc, acu_telcel, acu_correo, acu_parent) 
+                            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $query = $conexion->prepare($insertpadre);
+            $query->bind_param("issssssss", $idalumno, $datos['nompad'], $datos['cldopa'], $datos['docpad'], $datos['ciupad'], $datos['dirpad'], $datos['telpad'], $datos['corpad'], $datos['parpad'],);
+            $respuesta = $query->execute();
+            $insertmatricula = "INSERT INTO matriculas( id_alumno, id_grado, mat_valmat, mat_pensio, mat_saldo, mat_detalle) VALUES(?, ?, ?, ?, ?, ?)";
+            $query = $conexion->prepare($insertmatricula);
+            $query->bind_param("iissss",$idalumno, $datos['idgrado'], $datos['matric'], $datos['pensio'], $datos['abono'],$datos['detall'],);
             $respuesta = $query->execute();
             return $respuesta;
         }
