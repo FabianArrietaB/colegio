cf51aedbd4541d94052f167a77d2488649cd9db3
FabianArrietaB <FABIANARRIETA03@GMAIL.COMm>
2023-05-24 18:05:26 -0500

    2023

---
+++ gimamecol.sql
@@ -0,0 +1,558 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-05-2023 a las 01:05:08
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gimamecol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `acudientes`
+--
+
+CREATE TABLE `acudientes` (
+  `id_acudiente` int(11) NOT NULL,
+  `id_alumno` int(11) NOT NULL,
+  `id_operador` int(11) DEFAULT NULL,
+  `acu_nombre` varchar(45) NOT NULL,
+  `acu_cladoc` varchar(45) NOT NULL,
+  `acu_docume` varchar(45) NOT NULL,
+  `acu_ciudad` varchar(45) NOT NULL,
+  `acu_direcc` varchar(45) NOT NULL,
+  `acu_estrat` varchar(45) DEFAULT NULL,
+  `acu_telcel` varchar(45) NOT NULL,
+  `acu_correo` varchar(45) NOT NULL,
+  `acu_parent` varchar(45) NOT NULL,
+  `acu_estado` int(11) NOT NULL DEFAULT 1,
+  `acu_fecope` timestamp NOT NULL DEFAULT current_timestamp(),
+  `acu_fecupd` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `id_alumno` int(11) NOT NULL,
+  `id_grado` int(11) NOT NULL,
+  `id_operador` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `alu_nombre` varchar(45) NOT NULL,
+  `alu_cladoc` varchar(45) NOT NULL,
+  `alu_docume` varchar(45) NOT NULL,
+  `alu_sexo` varchar(45) NOT NULL,
+  `alu_gposan` varchar(45) NOT NULL,
+  `alu_factrh` varchar(45) NOT NULL,
+  `alu_ciudad` varchar(45) NOT NULL,
+  `alu_direcc` varchar(45) NOT NULL,
+  `alu_estrat` varchar(45) NOT NULL,
+  `alu_telcel` varchar(45) NOT NULL,
+  `alu_correo` varchar(45) NOT NULL,
+  `alu_estado` int(11) NOT NULL DEFAULT 1,
+  `alu_fecnac` date NOT NULL,
+  `alu_fecope` timestamp NOT NULL DEFAULT current_timestamp(),
+  `alu_fecupd` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auditorias`
+--
+
+CREATE TABLE `auditorias` (
+  `id_auditoria` int(11) NOT NULL,
+  `id_operador` int(11) NOT NULL,
+  `id_alumno` int(11) NOT NULL,
+  `id_grado` int(11) NOT NULL,
+  `id_tipopago` int(11) NOT NULL,
+  `aud_numdoc` varchar(45) NOT NULL,
+  `aud_valor` varchar(45) NOT NULL,
+  `aud_abono` varchar(45) NOT NULL,
+  `aud_fecope` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id_categoria` int(11) NOT NULL,
+  `id_operador` int(11) NOT NULL,
+  `cat_nombre` varchar(45) NOT NULL,
+  `cat_fecope` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `id_empleado` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_operador` int(11) NOT NULL,
+  `emp_nombre` varchar(45) NOT NULL,
+  `emp_cladoc` varchar(45) NOT NULL,
+  `emp_docume` varchar(45) NOT NULL,
+  `emp_cargo` varchar(45) NOT NULL,
+  `emp_telcel` varchar(45) NOT NULL,
+  `emp_ciudad` varchar(45) NOT NULL,
+  `emp_direcc` varchar(45) NOT NULL,
+  `emp_estrat` varchar(45) NOT NULL,
+  `emp_correo` varchar(45) NOT NULL,
+  `emp_tipcon` varchar(45) NOT NULL,
+  `emp_salari` varchar(45) NOT NULL,
+  `emp_codces` varchar(45) NOT NULL,
+  `emp_codeps` varchar(45) NOT NULL,
+  `emp_codpen` varchar(45) NOT NULL,
+  `emp_codarl` varchar(45) NOT NULL,
+  `emp_sexo` varchar(45) NOT NULL,
+  `emp_estciv` varchar(45) NOT NULL,
+  `emp_escola` varchar(45) NOT NULL,
+  `emp_gposan` varchar(45) NOT NULL,
+  `emp_factrh` varchar(45) NOT NULL,
+  `emp_hijos` varchar(45) NOT NULL,
+  `emp_estado` int(11) DEFAULT 1,
+  `emp_fecnac` datetime NOT NULL,
+  `emp_fecope` timestamp NOT NULL DEFAULT current_timestamp(),
+  `emp_fecupd` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grados`
+--
+
+CREATE TABLE `grados` (
+  `id_grado` int(11) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `id_operador` int(11) NOT NULL,
+  `gra_nombre` varchar(45) NOT NULL,
+  `gra_matric` varchar(45) NOT NULL,
+  `gra_pensio` varchar(45) NOT NULL,
+  `gra_canalu` bigint(10) NOT NULL,
+  `gra_estado` int(11) DEFAULT 1,
+  `gra_fecope` timestamp NOT NULL DEFAULT current_timestamp(),
+  `gra_fecupd` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `matriculas`
+--
+
+CREATE TABLE `matriculas` (
+  `id_matricula` int(11) NOT NULL,
+  `id_alumno` int(11) NOT NULL,
+  `id_grado` int(11) NOT NULL,
+  `id_operador` int(11) NOT NULL,
+  `id_tipopago` int(11) NOT NULL,
+  `id_tippagpen` int(11) NOT NULL,
+  `mat_numdoc` varchar(45) NOT NULL,
+  `mat_valmat` varchar(45) NOT NULL,
+  `mat_pensio` varchar(45) NOT NULL,
+  `mat_saldo` varchar(45) NOT NULL,
+  `mat_salpen` varchar(45) NOT NULL,
+  `mat_fecope` timestamp NOT NULL DEFAULT current_timestamp(),
+  `mat_fecmat` timestamp NOT NULL DEFAULT current_timestamp(),
+  `mat_fecpen` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pais`
+--
+
+CREATE TABLE `pais` (
+  `id_pais` int(11) NOT NULL,
+  `pais_nombre` varchar(45) NOT NULL,
+  `pais_fecope` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pais`
+--
+
+INSERT INTO `pais` (`id_pais`, `pais_nombre`, `pais_fecope`) VALUES
+(1, 'ALEMANIA', '2023-02-01 05:00:00'),
+(2, 'BRASIL', '2023-02-01 05:00:00'),
+(3, 'COLOMBIA', '2023-02-01 05:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `parafiscales`
+--
+
+CREATE TABLE `parafiscales` (
+  `id_parafiscales` int(11) NOT NULL,
+  `par_nombre` varchar(45) NOT NULL,
+  `par_fecope` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `parafiscales`
+--
+
+INSERT INTO `parafiscales` (`id_parafiscales`, `par_nombre`, `par_fecope`) VALUES
+(1, 'ALIANSALUD ENTIDAD PROMOTORA DE SALUD S.A.', '2023-05-13 01:27:08'),
+(2, 'CAPITAL SALUD', '2023-05-13 01:27:08'),
+(3, 'COMPENSAR   E.P.S.', '2023-05-13 01:27:08'),
+(4, 'FAMISANAR LTDA. ', '2023-05-13 01:27:08'),
+(5, 'NUEVA EPS S.A.', '2023-05-13 01:27:08'),
+(6, 'SANITAS S.A.', '2023-05-13 01:27:08'),
+(7, 'SALUD TOTAL S.A.  E.P.S.', '2023-05-13 01:27:08'),
+(8, 'SALUDVIDA S.A. E.P.S', '2023-05-13 01:27:08'),
+(9, 'SAVIA SALUD EPS', '2023-05-13 01:27:08'),
+(10, 'PROTECCION', '2023-05-13 01:27:08'),
+(11, 'PORVENIR', '2023-05-13 01:27:08'),
+(12, 'COLFONDOS', '2023-05-13 01:27:08'),
+(13, 'OLD MUTUAL', '2023-05-13 01:27:08'),
+(14, 'COLPENSIONES', '2023-05-13 01:27:08'),
+(15, '', '2023-05-13 02:34:28');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `id_operador` int(11) NOT NULL,
+  `pro_nombre` varchar(45) NOT NULL,
+  `pro_precio` varchar(45) NOT NULL,
+  `pro_estado` int(11) NOT NULL DEFAULT 1,
+  `pro_fecope` timestamp NOT NULL DEFAULT current_timestamp(),
+  `pro_fecupd` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int(11) NOT NULL,
+  `rol_nombre` varchar(45) NOT NULL,
+  `rol_descripcion` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `rol_nombre`, `rol_descripcion`) VALUES
+(1, 'Alumno', 'Es un Alumno'),
+(2, 'Docente', 'Es un Docente'),
+(3, 'Supervisor', 'Es un Surpervisor'),
+(4, 'Administrador', 'Es un Administrador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sedes`
+--
+
+CREATE TABLE `sedes` (
+  `id_sedes` int(11) NOT NULL,
+  `id_tipo` int(11) NOT NULL,
+  `id_operador` int(11) DEFAULT NULL,
+  `sed_razsoc` varchar(45) NOT NULL,
+  `sed_nombre` varchar(45) NOT NULL,
+  `sed_nit` varchar(45) NOT NULL,
+  `sed_correo` varchar(45) NOT NULL,
+  `sed_telcel` varchar(45) NOT NULL,
+  `sed_direcc` varchar(45) NOT NULL,
+  `sed_tipper` varchar(45) NOT NULL,
+  `sed_regime` varchar(45) NOT NULL,
+  `sed_pais` varchar(45) NOT NULL,
+  `sed_depart` varchar(45) NOT NULL,
+  `sed_muni` varchar(45) NOT NULL,
+  `sed_estado` varchar(45) NOT NULL DEFAULT '1',
+  `sed_fecope` timestamp NOT NULL DEFAULT current_timestamp(),
+  `sed_fecupd` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitudes`
+--
+
+CREATE TABLE `solicitudes` (
+  `id_solicitud` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_grado` int(11) NOT NULL,
+  `id_operador` int(11) NOT NULL DEFAULT 0,
+  `id_venta` int(11) NOT NULL DEFAULT 0,
+  `rep_tipo` varchar(45) NOT NULL,
+  `rep_detalle` varchar(250) NOT NULL,
+  `rep_estado` varchar(45) NOT NULL DEFAULT '0',
+  `rep_solucion` varchar(250) NOT NULL DEFAULT '0',
+  `rep_fecope` timestamp NOT NULL DEFAULT current_timestamp(),
+  `rep_fecupd` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `id_rol` int(11) NOT NULL DEFAULT 1,
+  `id_operador` int(11) NOT NULL,
+  `user_usuario` varchar(45) NOT NULL,
+  `user_nombre` varchar(100) NOT NULL,
+  `user_contra` varchar(255) NOT NULL,
+  `user_correo` varchar(45) NOT NULL,
+  `user_estado` int(11) NOT NULL DEFAULT 0,
+  `user_fecope` timestamp NOT NULL DEFAULT current_timestamp(),
+  `user_fecupd` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `id_rol`, `id_operador`, `user_usuario`, `user_nombre`, `user_contra`, `user_correo`, `user_estado`, `user_fecope`, `user_fecupd`) VALUES
+(1, 4, 1, 'Admin', 'Administrador', '202cb962ac59075b964b07152d234b70', 'admin@gmail.com', 1, '2023-01-01 05:00:00', '2023-01-01 05:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id_venta` int(11) NOT NULL,
+  `id_alumno` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_operador` int(11) NOT NULL,
+  `ven_numdoc` varchar(45) NOT NULL,
+  `ven_precio` varchar(45) NOT NULL,
+  `ven_fecope` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `acudientes`
+--
+ALTER TABLE `acudientes`
+  ADD PRIMARY KEY (`id_acudiente`);
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id_alumno`);
+
+--
+-- Indices de la tabla `auditorias`
+--
+ALTER TABLE `auditorias`
+  ADD PRIMARY KEY (`id_auditoria`);
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`id_empleado`);
+
+--
+-- Indices de la tabla `grados`
+--
+ALTER TABLE `grados`
+  ADD PRIMARY KEY (`id_grado`);
+
+--
+-- Indices de la tabla `matriculas`
+--
+ALTER TABLE `matriculas`
+  ADD PRIMARY KEY (`id_matricula`);
+
+--
+-- Indices de la tabla `pais`
+--
+ALTER TABLE `pais`
+  ADD PRIMARY KEY (`id_pais`);
+
+--
+-- Indices de la tabla `parafiscales`
+--
+ALTER TABLE `parafiscales`
+  ADD PRIMARY KEY (`id_parafiscales`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `sedes`
+--
+ALTER TABLE `sedes`
+  ADD PRIMARY KEY (`id_sedes`);
+
+--
+-- Indices de la tabla `solicitudes`
+--
+ALTER TABLE `solicitudes`
+  ADD PRIMARY KEY (`id_solicitud`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id_venta`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `acudientes`
+--
+ALTER TABLE `acudientes`
+  MODIFY `id_acudiente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id_alumno` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `auditorias`
+--
+ALTER TABLE `auditorias`
+  MODIFY `id_auditoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `grados`
+--
+ALTER TABLE `grados`
+  MODIFY `id_grado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `matriculas`
+--
+ALTER TABLE `matriculas`
+  MODIFY `id_matricula` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pais`
+--
+ALTER TABLE `pais`
+  MODIFY `id_pais` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `parafiscales`
+--
+ALTER TABLE `parafiscales`
+  MODIFY `id_parafiscales` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `sedes`
+--
+ALTER TABLE `sedes`
+  MODIFY `id_sedes` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `solicitudes`
+--
+ALTER TABLE `solicitudes`
+  MODIFY `id_solicitud` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
