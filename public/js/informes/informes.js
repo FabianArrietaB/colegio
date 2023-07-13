ebe7deba7cc3c1816b3f522f4510ba7f8e7f8bc1
FabianArrietaB <fabianarrieta03@gmail.com>
2023-07-12 21:34:59 -0500

    2023

--- public/js/informes/informes.js
+++ public/js/informes/informes.js
@@ -63,10 +63,9 @@ function detallefactura(idfacturas){
 
 function imprimir(){
     var factura = window.open("", "factura", "width=800, heigth=600");
-    factura.document.write("<html><head><title></title>");
     factura.document.write("<style><link rel='stylesheet' href='../../css/ticket.css'></style></head><body>");
     factura.document.write($("#conte-modal-factura").html());
-    factura.document.write("</body></html>");
+    factura.document.write("</body>");
     factura.document.close();
     factura.print();
 }
