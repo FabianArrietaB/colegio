bd227af9e9883a4065e1eebc8dfb23cd9405cba1
FabianArrietaB <fabianarrieta03@gmail.com>
2023-12-06 22:06:30 -0500

    2023

--- vista/facturar.php
+++ vista/facturar.php
@@ -5,7 +5,7 @@
 <section class="home-section">
     <div class="container-fluid">
         <div class="page-content">
-            <div class="card border-primary">
+            <fieldset class="group-border">
                 <div class="card-header text-center">
                     <div href="dashboard.php" class="title">
                         <h2>GENERAR FACTURA</h2>
@@ -14,17 +14,37 @@
             </div>
             <form id="frmcrearfactura" method="post" onsubmit="return crearfactura()">
                 <div class="row">
-                    <div class="col-9">
+                    <div class="col-6">
                         <div class="card border-primary">
                             <div class="title">
                                 <h4>INFORMACION CLIENTE</h4>
                             </div>
                         </div>
                     </div>
-                    <div class="col-3">
-                        <div class="card border-primary">
-                            <div class="title">
-                                <h4>INFORMACION FACTURA</h4>
+                    <div class="col-6">
+                        <div class="card border-primary ">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="inputGroup-sizing-default">Tipo Documento</span>
+                                        <select type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                            <option value="1">FACTURA VENTA</option>
+                                            <option value="2">FACTURA ELECTRONICA</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="inputGroup-sizing-default">Numero Documento</span>
+                                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="inputGroup-sizing-default">Fecha Documento</span>
+                                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -32,15 +52,31 @@
                 <div class="row">
                     <div class="col-9">
                         <div class="card border-primary">
-                            <div class="title">
-                                <h4>INFORMACION CLIENTE</h4>
+                            <div class="table-responsive justify-content-center">
+                                <table class="table table-light text-center">
+                                    <thead class="thead-light">
+                                        <tr>
+                                            <th>Item</th>
+                                            <th>CODIGO</th>
+                                            <th>NOMBRE PRODUCTO</th>
+                                            <th>CANTIDAD</th>
+                                            <th>PRECIO</th>
+                                            <th>PRECIO TOTAL</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
                     <div class="col-3">
                         <div class="card border-primary">
                             <div class="title">
-                                <h4>INFORMACION FACTURA</h4>
+                                <h4>TOTALES</h4>
                             </div>
                         </div>
                     </div>
